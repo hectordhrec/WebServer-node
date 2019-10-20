1c0a3e88d9e45a0bcb3fcf89b29c13c64bc35032
hectordhrec <hector.carabali@correounivalle.edu.co>
2019-10-20 13:45:38 -0500

    pagina usasndo como engine a hbs, como render por defecto, se le puden enviar variable

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const app = express();
 
 /* Carpeta publica todo lo que se ubique en ella va a se rde dominio publico*/
 app.use(express.static(__dirname + '/public'));
-
+/* Express HBS engine */
+app.set('view engine', 'hbs');
 
 /* Todas las peticiones por "/" pasaran por esta funcion */
-// app.get('/', (req, res) => {
-//     //res.send('Hola mundo');
+app.get('/', (req, res) => {
+    //res.send('Hola mundo');
 
-//     let salida = {
-//         nombre: 'hector',
-//         edad: 29,
-//         url: req.url
-//     }
-//     res.send(salida);
-// });
+    res.render('home', {
+        nombre: 'Hector',
+        anio: new Date().getFullYear()
+    });
+});
 
 app.listen(3000, () =>{
     console.log('Escuchando el puerto 3000');
