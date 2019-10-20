a4c49173792dd45a56659ba119ed821fb9e4dc1d
hectordhrec <hector.carabali@correounivalle.edu.co>
2019-10-20 16:45:41 -0500

    Utilización de hbs, se crea todo en formato .hbs, se usan helpers

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers/helpers'); //No se necesita variables, al importarla de ejecuta el código de hbs
 
 /* Carpeta publica todo lo que se ubique en ella va a se rde dominio publico*/
 app.use(express.static(__dirname + '/public'));
+
 /* Express HBS engine */
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 /* Todas las peticiones por "/" pasaran por esta funcion */
 app.get('/', (req, res) => {
-    //res.send('Hola mundo');
-
     res.render('home', {
         nombre: 'Hector',
-        anio: new Date().getFullYear()
+        titulo: 'Home'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        titulo: 'About'
     });
 });
 
