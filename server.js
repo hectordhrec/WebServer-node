b4f24d6769ef4d930538669f2ba341a5037308e4
hectordhrec <hector.carabali@correounivalle.edu.co>
2019-10-20 11:43:52 -0500

    creación de la una pagina statica

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
 
-/* Todas las peticiones por "/" pasaran por esta funcion */
-app.get('/', (req, res) => {
-    //res.send('Hola mundo');
+/* Carpeta publica todo lo que se ubique en ella va a se rde dominio publico*/
+app.use(express.static(__dirname + '/public'));
 
-    let salida = {
-        nombre: 'hector',
-        edad: 29,
-        url: req.url
-    }
-    res.send(salida);
-});
+
+/* Todas las peticiones por "/" pasaran por esta funcion */
+// app.get('/', (req, res) => {
+//     //res.send('Hola mundo');
+
+//     let salida = {
+//         nombre: 'hector',
+//         edad: 29,
+//         url: req.url
+//     }
+//     res.send(salida);
+// });
 
 app.listen(3000, () =>{
     console.log('Escuchando el puerto 3000');
