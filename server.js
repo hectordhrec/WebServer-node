e6b42f39ee1ab93e56eb0cb67382799175919bb2
hectordhrec <hector.carabali@correounivalle.edu.co>
2019-10-20 18:25:45 -0500

    ajustes para subir a heroku, puerto, ajuste package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers/helpers'); //No se necesita variables, al importarla de ejecuta el código de hbs
+const port = process.env.PORT || 3000; //Puerto para heroku de lo contrario se usa el seteado => 3000
 
 /* Carpeta publica todo lo que se ubique en ella va a se rde dominio publico*/
 app.use(express.static(__dirname + '/public'));
@@ -24,6 +25,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () =>{
-    console.log('Escuchando el puerto 3000');
+app.listen(port, () =>{
+    console.log(`Escuchando el puerto ${port}`);
 });
